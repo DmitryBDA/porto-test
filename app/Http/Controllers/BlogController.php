389a69cbf01053f1234b24e63272493c4c091fe3
dmitriy <da_belousov@list.ru>
2021-01-05 13:47:14 +0300

    init

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BlogController extends Controller
+{
+    public function index() {
+
+        $a = 3;
+
+        $b = $a;
+
+        return 'blog';
+    }
+}
